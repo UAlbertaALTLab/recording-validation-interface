1e5ad5a0ac750e6df88b659097c19c6350883453
Eddie Antonio Santos <easantos@ualberta.ca>
2018-03-01 15:50:27 -0700

    Take data in and display it on the front-page.

--- app.py
+++ app.py
@@ -3,12 +3,30 @@
 
 # Copyright © 2018 Eddie Antonio Santos. All rights reserved.
 
-from flask import Flask  # type: ignore
+
+from flask import Flask, render_template  # type: ignore
+from flask_sqlalchemy import SQLAlchemy  # type: ignore
 
 
 app = Flask(__name__)
+# XXX: temporary location for directory
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Recording(db.Model):  # type: ignore
+    """
+    Represents a recording of a phrase (word or sentence).
+    """
+    file_path = db.Column(db.Text(), primary_key=True)
+    transcription = db.Column(db.Text())
+    translation = db.Column(db.Text())
+    speaker = db.Column(db.Text())
 
 
 @app.route('/')
-def hello():
-    return '<h1> Hello, World </h1>'
+def big_list():
+    return render_template(
+        'index.html',
+        recordings=Recording.query.all()
+    )
---
+++ create_db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Copyright © 2018 Eddie Antonio Santos. All rights reserved.
+
+"""
+Creates the database from scratch.
+"""
+
+import sys
+from pathlib import Path
+
+from app import db, Recording
+from recval.parse_index import parse
+
+index_file = Path('./data/samples/index.html')
+
+if __name__ == '__main__':
+    db.create_all()
+
+    if 'create' in sys.argv[1:]:
+        # Assumption is that parse() returns normalized data.
+        recordings, _words, _sentences = parse(index_file)
+        for crk, en, speaker, recording_path in recordings:
+            db.session.add(
+                Recording(file_path=recording_path,
+                          translation=en,
+                          transcription=crk,
+                          speaker=speaker)
+            )
+        db.session.commit()
