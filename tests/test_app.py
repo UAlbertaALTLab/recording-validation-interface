c6b95b35a2a919fc57588fac2e7dd2c9873ccec5
Eddie Antonio Santos <easantos@ualberta.ca>
2018-03-09 09:54:13 -0700

    Green.

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,11 +9,11 @@ import pytest  # type: ignore
 from flask import url_for  # type: ignore
 
 
-def test_index(selenium, my_live_server):
+def test_index(selenium, live_server):
     """
     See if I find a Cree word on the page.
     """
-    url = my_live_server.url(url_for('list_all_words'))
+    url = live_server.url(url_for('list_all_words'))
     selenium.get(url)
     elem = selenium.find_element_by_xpath(
         "//*[contains(text(), 'cakayikan')]"
@@ -23,17 +23,14 @@ def test_index(selenium, my_live_server):
     assert elem.tag_name == 'td'
 
 
-def test_word_related_to_sentence(selenium, my_live_server):
+def test_word_related_to_sentence(client):
     """
     Determine whether a Cree word is related to a sentence.
     """
-    url = my_live_server.url(url_for('list_all_words'))
-    selenium.get(url)
+    rv = client.get(url_for('list_all_words'))
+    assert rv.status_code == 200
+
     sentence = normalize('NFC', "êwitikocik êkwa êwitikopêcik")
-    elem = selenium.find_element_by_xpath(
-        f"//*[contains(text(), '{sentence}')]"
-    )
-    # There must be at LEAST one element!
-    assert elem.is_displayed()
+    assert sentence.encode('UTF-8') in rv.data
 
     # TODO: assert that it's associated with a word
