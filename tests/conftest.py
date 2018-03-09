1d63c7130298cc1253a06ed4b39c1b76ae7341bc
Eddie Antonio Santos <easantos@ualberta.ca>
2018-03-09 09:47:15 -0700

    Flail around with pytest-flask. Failed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import socket
 import sys
 from pathlib import Path
 
 import pytest  # type: ignore
+from pytest_flask.fixtures import LiveServer  # type: ignore
 
 
 # XXX: gross sys.path manipulation to allow us to
@@ -24,3 +26,69 @@ def app():
 
     from recval.app import app
     return app
+
+
+# my_live_server code adpated from https://github.com/pytest-dev/pytest-flask/blob/master/pytest_flask/fixtures.py
+
+# The MIT License (MIT)
+#
+# Copyright © 2014–2016 Vital Kudzelka and contributors.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+@pytest.fixture(scope='function')
+def my_live_server(request, app, monkeypatch):
+    """Run application in a separate process.
+
+    When the ``live_server`` fixture is applyed, the ``url_for`` function
+    works as expected::
+
+        def test_server_is_up_and_running(live_server):
+            index_url = url_for('index', _external=True)
+            assert index_url == 'http://localhost:5000/'
+            res = urllib2.urlopen(index_url)
+            assert res.code == 200
+
+    """
+    # Get an open port. There's technically a race condition here, but
+    # whatever.
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 0))
+    port = s.getsockname()[1]
+    s.close()
+
+    # Explicitly set application ``SERVER_NAME`` for test suite
+    # and restore original value on test teardown.
+    server_name = app.config['SERVER_NAME'] or 'localhost'
+    monkeypatch.setitem(app.config, 'SERVER_NAME',
+                        _rewrite_server_name(server_name, str(port)))
+
+    server = LiveServer(app, port)
+    server.start()
+    yield server
+    server.stop()
+
+
+def _rewrite_server_name(server_name, new_port):
+    """Rewrite server port in ``server_name`` with ``new_port`` value."""
+    sep = ':'
+    if sep in server_name:
+        server_name, port = server_name.split(sep, 1)
+    return sep.join((server_name, new_port))
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,22 +3,37 @@
 
 # Copyright © 2018 Eddie Antonio Santos. All rights reserved.
 
+from unicodedata import normalize
 
 import pytest  # type: ignore
 from flask import url_for  # type: ignore
 
-# Start a Flask server for the tests in this module.
-pytestmark = pytest.mark.usefixtures("live_server")
 
-
-def test_index(selenium):
+def test_index(selenium, my_live_server):
     """
     See if I find a Cree word on the page.
     """
-    selenium.get(url_for('list_all_words', _external=True))
+    url = my_live_server.url(url_for('list_all_words'))
+    selenium.get(url)
     elem = selenium.find_element_by_xpath(
         "//*[contains(text(), 'cakayikan')]"
     )
     # There must be at LEAST one element!
     assert elem.is_displayed()
     assert elem.tag_name == 'td'
+
+
+def test_word_related_to_sentence(selenium, my_live_server):
+    """
+    Determine whether a Cree word is related to a sentence.
+    """
+    url = my_live_server.url(url_for('list_all_words'))
+    selenium.get(url)
+    sentence = normalize('NFC', "êwitikocik êkwa êwitikopêcik")
+    elem = selenium.find_element_by_xpath(
+        f"//*[contains(text(), '{sentence}')]"
+    )
+    # There must be at LEAST one element!
+    assert elem.is_displayed()
+
+    # TODO: assert that it's associated with a word
