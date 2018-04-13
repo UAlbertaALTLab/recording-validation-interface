5e4f855d393cadd0af8590471781a9e8ad317883
Eddie Antonio Santos <easantos@ualberta.ca>
2018-04-13 08:50:00 -0600

    Wrote a test?

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,7 @@ from unicodedata import normalize
 
 import pytest  # type: ignore
 from flask import url_for  # type: ignore
+from bs4 import BeautifulSoup
 
 
 def test_index(selenium, live_server):
@@ -29,8 +30,21 @@ def test_word_related_to_sentence(client):
     """
     rv = client.get(url_for('list_all_words'))
     assert rv.status_code == 200
+    webpage = rv.data.decode('UTF-8')
 
-    sentence = normalize('NFC', "êwitikocik êkwa êwitikopêcik")
-    assert sentence.encode('UTF-8') in rv.data
+    word = nfc("Nanapowapis")
+    assert word in webpage
+
+    sentence = nfc("kayas mana ekinanapwapitacik otemwawa ekosi namoya wahyaw ketohteyiwa")
+    assert sentence in webpage
 
     # TODO: assert that it's associated with a word
+    soup = BeautifulSoup(webpage, 'lxml')
+    tags = soup.find_all(string=word)
+    assert len(tags) == 1
+    td, = tags
+    assert td.name == 'td'
+
+
+def nfc(text: str) -> str:
+    return normalize('NFC', text)
