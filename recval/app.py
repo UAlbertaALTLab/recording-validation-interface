87384bb941418e86665df9c04c340ff3ab4cf076
Eddie Antonio Santos <easantos@ualberta.ca>
2018-04-17 13:00:44 -0600

    Implement simple pagination.

--- recval/app.py
+++ recval/app.py
@@ -218,7 +218,7 @@ def list_all_words():
     query = Phrase.query.options(subqueryload(Phrase.recordings))
     return render_template(
         'index.html',
-        phrases=query.limit(10).all()
+        page=query.paginate()
     )
 
 
