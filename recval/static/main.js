f4e5de74ae7c32d0fda89832f46c2c74b6ec545b
Eddie Antonio Santos <easantos@ualberta.ca>
2018-04-17 22:58:28 -0600

    Add super basic transliteration/translation update support.

---
+++ recval/static/main.js
@@ -0,0 +1,42 @@
+$(function () {
+  $('main > table').click(function (evt) {
+    var $el = $(evt.target);
+    var field;
+    field = $el.is('[lang=en]') && 'translation';
+    field = field || ($el.is('[lang=crk]') && 'transcription');
+
+    if (!field) {
+      return;
+    }
+
+    var updateURI = $el.parent('[data-update-uri]').data('update-uri');
+
+    if (!updateURI) {
+      console.error('No update URI found for ', event.target);
+    }
+
+    var original = $el.text();
+
+    /* We clicked on a translation or transliteration. Ask to change it. */
+    var value = prompt('Change "' + original + '" to:', original);
+    if (!value) {
+      return;
+    }
+
+    fetch(updateURI, {
+      method: 'PATCH',
+      body: JSON.stringify({ field: field, value: value }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(function (res) {
+      if (res.ok) {
+        $el.text(value);
+      } else {
+        alert('Could not change text for some reason.');
+      }
+    });
+  });
+});
+/* eslint-env: browser, jquery */
+/* global $ */
