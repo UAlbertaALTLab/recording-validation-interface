8fcfbe41b4d9c24700c82b3e19a2490083c7b03b
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-01 14:38:49 -0600

    Do not explicitly normalize.

--- create_db.py
+++ create_db.py
@@ -12,7 +12,9 @@ from pathlib import Path
 
 from tqdm import tqdm  # type: ignore
 
-from recval.model import db, Recording, Word, Sentence, normalize_utterance
+from recval.model import db, Recording, Word, Sentence
+from recval.parse_index import parse
+
 
 here = Path('.')
 SOURCE_AUDIO_PATH = here / 'data' / 'samples'
@@ -21,7 +23,6 @@ assert SOURCE_AUDIO_PATH.resolve().is_dir()
 
 if __name__ == '__main__':
     from recval.app import app
-    from recval.parse_index import parse
 
     index_file = Path('./data/samples/index.html')
     assert index_file.exists()
@@ -32,8 +33,7 @@ if __name__ == '__main__':
         # Assumption is that parse() returns normalized data.
         recordings, _words, _sentences = parse(index_file)
         for crk, en, speaker, recording_path in tqdm(recordings):
-            word = Word(translation=normalize_utterance(en),
-                        transcription=normalize_utterance(crk))
+            word = Word(translation=en, transcription=crk)
             recording = Recording.new(phrase=word,
                                       input_file=SOURCE_AUDIO_PATH / recording_path,
                                       speaker=speaker)
