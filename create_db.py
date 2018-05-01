e5aab4d3c57ad3dc610334878193c62cda2f7542
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-01 14:24:12 -0600

    Make create_db app aware.

--- create_db.py
+++ create_db.py
@@ -12,6 +12,7 @@ from pathlib import Path
 
 from tqdm import tqdm  # type: ignore
 
+from recval.model import db, Recording, Word, Sentence, normalize_utterance
 
 here = Path('.')
 SOURCE_AUDIO_PATH = here / 'data' / 'samples'
@@ -19,21 +20,22 @@ assert SOURCE_AUDIO_PATH.resolve().is_dir()
 
 
 if __name__ == '__main__':
-    from recval.app import db, Recording, Word, Sentence, normalize_utterance
+    from recval.app import app
     from recval.parse_index import parse
 
     index_file = Path('./data/samples/index.html')
     assert index_file.exists()
 
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
-    # Assumption is that parse() returns normalized data.
-    recordings, _words, _sentences = parse(index_file)
-    for crk, en, speaker, recording_path in tqdm(recordings):
-        word = Word(translation=normalize_utterance(en),
-                    transcription=normalize_utterance(crk))
-        recording = Recording.new(phrase=word,
-                                  input_file=SOURCE_AUDIO_PATH / recording_path,
-                                  speaker=speaker)
-        db.session.add(recording)
-        db.session.commit()
+        # Assumption is that parse() returns normalized data.
+        recordings, _words, _sentences = parse(index_file)
+        for crk, en, speaker, recording_path in tqdm(recordings):
+            word = Word(translation=normalize_utterance(en),
+                        transcription=normalize_utterance(crk))
+            recording = Recording.new(phrase=word,
+                                      input_file=SOURCE_AUDIO_PATH / recording_path,
+                                      speaker=speaker)
+            db.session.add(recording)
+            db.session.commit()
