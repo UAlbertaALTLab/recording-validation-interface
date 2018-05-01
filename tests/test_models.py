aa17785cf9b949e5690d756f72124db4a745791b
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-01 15:33:24 -0600

    Recreate the DB every test.

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,8 @@ from pathlib import Path
 import pytest  # type: ignore
 from sqlalchemy.schema import MetaData, DropConstraint  # type: ignore
 
-from recval.model import db, Word, Sentence, Recording
+from recval.model import Word, Sentence, Recording
+from recval.model import db as _db
 from recval.app import app as _app
 
 
@@ -17,28 +18,11 @@ assert TEST_WAV.exists()
 # Based on http://alextechrants.blogspot.ca/2014/01/unit-testing-sqlalchemy-apps-part-2.html
 
 
-@pytest.fixture(scope='session')
-def app():
-    with _app.app_context():
-        yield _app
+def test_insert_word(db):
+    """
+    Insert a word, and retrieve it again.
+    """
 
-
-@pytest.fixture(scope='session', autouse=True)
-def setup_db(app):
-    with tempfile.TemporaryDirectory() as d:
-        path = Path(d) / 'test.db'
-        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path!s}'
-
-        db.create_all()
-        db.session.flush()
-        db.session.expunge_all()
-        db.session.commit()
-
-        yield
-
-
-def test_insert_word(app):
-    # TODO: test unnormalized word.
     word = Word(transcription=' acimosis', translation='puppy ')
     recording = Recording.new(phrase=word, input_file=TEST_WAV, speaker='NIL')
     db.session.add(recording)
@@ -51,3 +35,23 @@ def test_insert_word(app):
     result_set = Word.query.filter(Word.transcription == 'acimosis').all()
     assert len(result_set) >= 1
     assert word in result_set
+
+
+@pytest.fixture()
+def app():
+    with _app.app_context():
+        yield _app
+
+
+@pytest.fixture()
+def db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///:memory:'
+
+    _db.create_all()
+    _db.session.flush()
+    _db.session.expunge_all()
+    _db.session.commit()
+
+    yield _db
+
+    _db.session.close()
