95dfb77d9b4fba8032c8942d0cf32d6123552733
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-08 12:37:30 -0600

    Refactor: create class for extracting phrases.

--- extraction-scripts/find_sessions.py
+++ extraction-scripts/find_sessions.py
@@ -42,98 +42,11 @@ cree_pattern = re.compile(r'\b(?:cree|crk)\b', re.IGNORECASE)
 english_pattern = re.compile(r'\b(?:english|eng|en)\b', re.IGNORECASE)
 
 
-def extract_items(sound: AudioSegment,
-                  text_grid: TextGrid,
-                  session: RecordingSession,
-                  speaker: str,  # Something like "ABC"
-                  ) -> None:
-    assert len(text_grid.tiers) >= 2, "TextGrid has too few tiers"
-
-    cree_word_intervals = text_grid.tiers[WORD_TIER_CREE]
-    assert cree_pattern.search(cree_word_intervals.name)
-
-    english_word_intervals = text_grid.tiers[WORD_TIER_ENGLISH]
-    assert english_pattern.search(english_word_intervals.name)
-
-    cree_sentence_intervals = text_grid.tiers[SENTENCE_TIER_CREE]
-    assert cree_pattern.search(cree_sentence_intervals.name)
-
-    info(' ... ... extracting words')
-    for interval in cree_word_intervals:
-        if not interval.mark or interval.mark.strip() == '':
-            # This interval is empty, for some reason.
-            continue
-
-        transcription = normalize(interval.mark)
-
-        start = to_milliseconds(interval.minTime)
-        end = to_milliseconds(interval.maxTime)
-        midtime = (interval.minTime + interval.maxTime) / 2
-
-        # Figure out if this word belongs to a sentence.
-        sentence = cree_sentence_intervals.intervalContaining(midtime)
-        is_sentence = sentence and sentence.mark != ''
-
-        if is_sentence:
-            # It's an example sentence; leave it for the next loop.
-            continue
-
-        # Get the word's English gloss.
-        english_interval = english_word_intervals.intervalContaining(midtime)
-        translation = normalize(english_interval.mark)
-
-        # Snip out the sounds.
-        sound_bite = sound[start:end]
-        # tmills: normalize sound levels (some speakers are very quiet)
-        sound_bite.normalize(headroom=0.1)  # dB
-
-        # Export it.
-        slug = slugify(f"word-{transcription}-{session}-{speaker}-{start}",
-                       to_lower=True)
-        sound_bite.export(str(Path('/tmp') / f"{slug}.wav"))
-
-        # TODO: yield word
-
-    info(' ... ... extracting sentences')
-    english_sentence_intervals = text_grid.tiers[SENTENCE_TIER_ENGLISH]
-    assert english_pattern.search(english_sentence_intervals.name)
-
-    for interval in cree_sentence_intervals:
-        if not interval.mark or interval.mark.strip() == '':
-            # This interval is empty, for some reason.
-            continue
-
-        transcription = normalize(interval.mark)
-
-        start = to_milliseconds(interval.minTime)
-        end = to_milliseconds(interval.maxTime)
-        midtime = (interval.minTime + interval.maxTime) / 2
-
-        # Get the sentencs's English translation.
-        english_interval = english_word_intervals.intervalContaining(midtime)
-        translation = normalize(english_interval.mark)
-
-        # Snip out the sounds.
-        sound_bite = sound[start:end]
-        # tmills: normalize sound levels (some speakers are very quiet)
-        sound_bite.normalize(headroom=0.1)  # dB
-
-        # Export it.
-        slug = slugify(f"sentence-{transcription}-{session}-{speaker}-{start}",
-                       to_lower=True)
-        sound_bite.export(str(Path('/tmp') / f"{slug}.wav"))
-
-        # TODO: yield sentence.
-
-
-def to_milliseconds(seconds: Decimal) -> int:
-    """
-    Converts interval times to an integer in milliseconds.
-    """
-    return int(seconds * 1000)
-
-
 class RecordingExtractor:
+    """
+    Extracts recordings from a directory of sessions.
+    """
+
     def __init__(self) -> None:
         self.sessions: Dict[RecordingSession, Path] = {}
 
@@ -161,6 +74,8 @@ class RecordingExtractor:
         info(' ... Scanning %s for .TextGrid files', session_dir)
         text_grids = list(session_dir.glob('*.TextGrid'))
         info(' ... %d text grids', len(text_grids))
+
+        extractor = PhraseExtractor(session)
         for text_grid in text_grids:
             sound_file = text_grid.with_suffix('.wav')
             # TODO: tmill's kludge for certain missing filenames???
@@ -177,10 +92,107 @@ class RecordingExtractor:
 
             info(' ... ... Extract items from %s using speaker ID %s',
                  sound_file, speaker)
+            extractor.extract_all(AudioSegment.from_file(str(sound_file)),
+                                  TextGrid.fromFile(str(text_grid)),
+                                  speaker)
 
-            extract_items(AudioSegment.from_file(str(sound_file)),
-                          TextGrid.fromFile(str(text_grid)),
-                          session, speaker)
+
+class PhraseExtractor:
+    """
+    Extracts recorings from a session directory.
+    """
+    def __init__(self, session: RecordingSession) -> None:
+        self.session = session
+
+    def extract_all(self,
+                    sound: AudioSegment,
+                    text_grid: TextGrid,
+                    speaker: str,  # Something like "ABC"
+                    ) -> None:
+        assert len(text_grid.tiers) >= 2, "TextGrid has too few tiers"
+
+        cree_word_intervals = text_grid.tiers[WORD_TIER_CREE]
+        assert cree_pattern.search(cree_word_intervals.name)
+
+        english_word_intervals = text_grid.tiers[WORD_TIER_ENGLISH]
+        assert english_pattern.search(english_word_intervals.name)
+
+        cree_sentence_intervals = text_grid.tiers[SENTENCE_TIER_CREE]
+        assert cree_pattern.search(cree_sentence_intervals.name)
+
+        info(' ... ... extracting words')
+        for interval in cree_word_intervals:
+            if not interval.mark or interval.mark.strip() == '':
+                # This interval is empty, for some reason.
+                continue
+
+            transcription = normalize(interval.mark)
+
+            start = to_milliseconds(interval.minTime)
+            end = to_milliseconds(interval.maxTime)
+            midtime = (interval.minTime + interval.maxTime) / 2
+
+            # Figure out if this word belongs to a sentence.
+            sentence = cree_sentence_intervals.intervalContaining(midtime)
+            is_sentence = sentence and sentence.mark != ''
+
+            if is_sentence:
+                # It's an example sentence; leave it for the next loop.
+                continue
+
+            # Get the word's English gloss.
+            english_interval = english_word_intervals.intervalContaining(midtime)
+            translation = normalize(english_interval.mark)
+
+            # Snip out the sounds.
+            sound_bite = sound[start:end]
+            # tmills: normalize sound levels (some speakers are very quiet)
+            sound_bite.normalize(headroom=0.1)  # dB
+
+            # Export it.
+            slug = slugify(f"word-{transcription}-{self.session}-{speaker}-{start}",
+                           to_lower=True)
+            sound_bite.export(str(Path('/tmp') / f"{slug}.wav"))
+
+            # TODO: yield word
+
+        info(' ... ... extracting sentences')
+        english_sentence_intervals = text_grid.tiers[SENTENCE_TIER_ENGLISH]
+        assert english_pattern.search(english_sentence_intervals.name)
+
+        for interval in cree_sentence_intervals:
+            if not interval.mark or interval.mark.strip() == '':
+                # This interval is empty, for some reason.
+                continue
+
+            transcription = normalize(interval.mark)
+
+            start = to_milliseconds(interval.minTime)
+            end = to_milliseconds(interval.maxTime)
+            midtime = (interval.minTime + interval.maxTime) / 2
+
+            # Get the sentencs's English translation.
+            english_interval = english_word_intervals.intervalContaining(midtime)
+            translation = normalize(english_interval.mark)
+
+            # Snip out the sounds.
+            sound_bite = sound[start:end]
+            # tmills: normalize sound levels (some speakers are very quiet)
+            sound_bite.normalize(headroom=0.1)  # dB
+
+            # Export it.
+            slug = slugify(f"sentence-{transcription}-{self.session}-{speaker}-{start}",
+                           to_lower=True)
+            sound_bite.export(str(Path('/tmp') / f"{slug}.wav"))
+
+            # TODO: yield sentence.
+
+
+def to_milliseconds(seconds: Decimal) -> int:
+    """
+    Converts interval times to an integer in milliseconds.
+    """
+    return int(seconds * 1000)
 
 
 if __name__ == '__main__':
