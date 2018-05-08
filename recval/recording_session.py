ab46b9e056abbdb0988dd93d87061a96378db60d
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-08 12:11:01 -0600

    Extract RecordingSession from clean-sessions-dir.

---
+++ recval/recording_session.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Copyright © Eddie Antonio Santos. All rights reserved.
+
+import re
+from datetime import date as datetype
+from datetime import datetime
+from enum import Enum, auto
+from pathlib import Path
+from typing import AnyStr, Callable, Match, NamedTuple, Optional, TypeVar
+
+# This a strict pattern, that differs from the one in etc/;
+# It matches only directories with the normalized naming conventions.
+directory_pattern = re.compile(r'''
+    \A
+    (?P<isodate>
+        \d{4}-\d{1,2}-\d{1,2}   (?# This is slightly inaccurate, but it's okay. )
+    )
+    -
+    (?:
+        (?P<time_of_day> AM|PM) | __
+    )
+    -
+    (?:
+        (?P<location> US|DS|KCH|OFF) |  ___
+    )
+    -
+    (?P<subsession>
+        \d+
+    )
+    \Z
+''', re.VERBOSE)
+
+
+# ############################### Exceptions ############################# #
+
+
+class SessionParseError(ValueError):
+    """
+    Thrown when a session cannot be parsed.
+    """
+
+
+class InvalidTimeOfDayError(SessionParseError):
+    def __init__(self, code: str) -> None:
+        super().__init__(f"Unknown time of day: {code!r}")
+
+
+class InvalidLocationError(SessionParseError):
+    def __init__(self, code: str) -> None:
+        super().__init__(f'Unknown location code: {code!r}')
+
+
+# ############################## Enumerations ############################ #
+
+
+class TimeOfDay(Enum):
+    MORNING = 'AM'
+    AFTERNOON = 'PM'
+
+    @staticmethod
+    def parse(text: str) -> 'TimeOfDay':
+        code = text.upper()
+        if code == 'AM':
+            return TimeOfDay.MORNING
+        elif code == 'PM':
+            return TimeOfDay.AFTERNOON
+        raise InvalidTimeOfDayError(text)
+
+
+class Location(Enum):
+    # For the first three years of recording:
+    DOWNSTAIRS = 'DS'  # Downstairs in Sohki house
+    UPSTAIRS = 'US'  # Upstairs in Sohki house
+    # For the last academic year of recording:
+    KITCHEN = 'KCH'  # Kitchen in Miyo head office
+    OFFICE = 'OFF'  # Office in Miyo head office
+
+    @staticmethod
+    def parse(text: str) -> 'Location':
+        code = text.upper()
+        if code in ('KCH', 'KIT'):
+            return Location.KITCHEN
+        elif code == 'OFF':
+            return Location.OFFICE
+        elif code in ('DS', 'DOWNSTAIRS'):
+            return Location.DOWNSTAIRS
+        elif code in ('US', 'UPSTAIRS'):
+            return Location.UPSTAIRS
+        raise InvalidLocationError(text)
+
+
+class RecordingSession(NamedTuple):
+    date: datetype
+    time_of_day: Optional[TimeOfDay]
+    subsession: Optional[int]
+    location: Optional[Location]
+
+    @property
+    def year(self):
+        return self.date.year
+
+    @property
+    def month(self):
+        return self.date.month
+
+    @property
+    def day(self):
+        return self.date.day
+
+    @classmethod
+    def from_name(cls, directory_name: str) -> 'RecordingSession':
+        m = directory_pattern.match(directory_name)
+        if m is None:
+            raise SessionParseError(f"directory does not match pattern {directory_name}")
+
+        # Parse out all the things into appropriate datatypes.
+        date = datetime.strptime(m.group('isodate'), '%Y-%m-%d').date()
+        time_of_day = apply_or_none(TimeOfDay.parse, m.group('time_of_day'))
+        subsession = apply_or_none(int, m.group('subsession'))
+        location = apply_or_none(Location.parse, m.group('location'))
+
+        return cls(date=date,
+                   time_of_day=time_of_day,
+                   subsession=subsession,
+                   location=location)
+
+    def as_filename(self) -> str:
+        """
+        Returns a standardized filename for any session.
+        """
+        time = (self.time_of_day and self.time_of_day.value) or '__'
+        loc = (self.location and self.location.value) or '___'
+        subsesh = self.subsession or 0
+        return f"{self.date:%Y-%m-%d}-{time}-{loc}-{subsesh:1d}"
+
+
+T = TypeVar('T')
+
+
+def apply_or_none(fn: Callable[[AnyStr], T],
+                  match: Optional[AnyStr]) -> Optional[T]:
+    """
+    Applies fn to the match ONLY if the match is not None.
+
+    This is similar to >>= from Haskell.
+    """
+    if match is not None:
+        return fn(match)
+    return None
---
+++ tests/test_recording_session.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Copyright © Eddie Antonio Santos. All rights reserved.
+
+import pytest  # type: ignore
+from recval.recording_session import RecordingSession, TimeOfDay, Location
+from recval.recording_session import SessionParseError
+
+
+def test_does_not_parse_arbitrary_directory_name():
+    with pytest.raises(SessionParseError):
+        RecordingSession.from_name('sagiesbr-annotations2')
+
+
+def test_basic_parsing():
+    s = RecordingSession.from_name('2018-01-24-AM-KCH-2')
+    assert isinstance(s, RecordingSession)
+    assert s.year == 2018
+    assert s.month == 1
+    assert s.day == 24
+    assert s.time_of_day == TimeOfDay.MORNING
+    assert s.subsession == 2
+    assert s.location == Location.KITCHEN
+    assert s.as_filename() == '2018-01-24-AM-KCH-2'
+
+
+def test_trickier_parse():
+    s = RecordingSession.from_name('2016-11-07-PM-DS-0')
+    assert s.time_of_day == TimeOfDay.AFTERNOON
+    assert s.location == Location.DOWNSTAIRS
+    assert s.as_filename() == '2016-11-07-PM-DS-0'
+
+
+def test_location_missing():
+    s = RecordingSession.from_name('2016-01-18-AM-___-0')
+    assert s.time_of_day == TimeOfDay.MORNING
+    assert s.location is None
+    assert s.as_filename() == '2016-01-18-AM-___-0'
+
+
+def test_time_of_day_missing():
+    s = RecordingSession.from_name('2016-01-18-__-OFF-0')
+    assert s.time_of_day is None
+    assert s.location is Location.OFFICE
+    assert s.as_filename() == '2016-01-18-__-OFF-0'
