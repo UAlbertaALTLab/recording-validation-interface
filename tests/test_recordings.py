6131022cf89a6b1760cce6b3633b8b54050b3697
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-21 16:52:53 -0600

    Test that it's an m4a file.

--- tests/test_recordings.py
+++ tests/test_recordings.py
@@ -14,9 +14,16 @@ from recval.transcode_recording import transcode_to_aac
 def test_can_transcode_wave_file(wave_file_path: Path,
                                  temporary_directory: Path) -> None:
     destination = temporary_directory / f"{uuid4()}.m4a"
+
+    # Check that transcoding creates a new file, at least.
     assert not destination.exists()
     transcode_to_aac(wave_file_path, destination)
     assert destination.exists()
+
+    # Check that it's has an MP4 header, at least.
+    blob = destination.read_bytes()
+    assert b'ftyp' == blob[4:8]
+    assert b'M4A ' == blob[8:12]
 
 
 @pytest.fixture
