70b7de18ad21472a3f5c38439cbe1085940b6f6d
Eddie Antonio Santos <easantos@ualberta.ca>
2018-05-29 18:37:31 -0600

    Do not export ANYTHING from recval.cli.

--- recval/cli/__init__.py
+++ recval/cli/__init__.py
@@ -13,6 +13,9 @@ from .user import user_cli
 from .db import db_cli
 from .data import data_cli
 
+# Export NOTHING. This is important!
+__all__ = []  # type: ignore
+
 app.cli.add_command(user_cli)
 app.cli.add_command(db_cli)
 app.cli.add_command(data_cli)
