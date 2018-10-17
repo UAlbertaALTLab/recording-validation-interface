a24d6a9af2625caff59f15a8a57c9bac0d10ee01
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-17 15:08:17 -0600

    Remove user cli commands.

--- recval/cli/__init__.py
+++ recval/cli/__init__.py
@@ -21,13 +21,11 @@ Add user and database management commands to the Flask CLI.
 
 from recval.app import app
 
-from .user import user_cli
 from .db import db_cli
 from .metadata import data_cli
 
 # Export NOTHING. This is important!
 __all__ = []  # type: ignore
 
-app.cli.add_command(user_cli)
 app.cli.add_command(db_cli)
 app.cli.add_command(data_cli)
