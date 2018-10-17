7be98de6a80a6ee398de9c54000f7c43f186ec62
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-17 16:00:12 -0600

    Convert FlaskSQLAlchemyImporter thing into a function.

--- recval/init_db.py
+++ recval/init_db.py
@@ -27,36 +27,35 @@ def initialize(directory: Path) -> None:
     )
 
 
-class FlaskSQLAlchemyRecordingImporter:
+def FlaskSQLAlchemyRecordingImporter():
     """
     Naming things is hard
     """
 
-    def __init__(self) -> None:
-        from recval.database import init_db
-        self.info2phrase = {}  # type: ignore
-        self.db = init_db()
+    from recval.database import init_db
+    from recval.model import Phrase, VersionedString
+    from recval.model import Phrase, Word, Sentence
+    from recval.model import Phrase, VersionedString
 
-    if TYPE_CHECKING:
-        from recval.model import Phrase, VersionedString
+    info2phrase = {}  # type: ignore
+    db = init_db()
 
-    def make_phrase(self, info: RecordingInfo) -> Phrase:
+    def make_phrase(info: RecordingInfo) -> Phrase:
         """
         Tries to fetch an existing phrase from the database, or else creates a
         new one.
         """
-        from recval.model import Phrase, Word, Sentence
 
         key = info.type, info.transcription
 
-        if key in self.info2phrase:
+        if key in info2phrase:
             # Look up the phrase.
-            phrase_id = self.info2phrase[key]
+            phrase_id = info2phrase[key]
             return Phrase.query.filter_by(id=phrase_id).one()
 
         # Otherwise, create a new phrase.
-        transcription = self.fetch_versioned_string(info.transcription)
-        translation = self.fetch_versioned_string(info.translation)
+        transcription = fetch_versioned_string(info.transcription)
+        translation = fetch_versioned_string(info.translation)
         if info.type == 'word':
             p = Word(transcription_meta=transcription,
                      translation_meta=translation)
@@ -66,17 +65,16 @@ class FlaskSQLAlchemyRecordingImporter:
         else:
             raise Exception(f"Unexpected phrase type: {info.type!r}")
         assert p.id is None
-        self.db.session.add(p)
-        self.db.session.commit()
+        db.session.add(p)
+        db.session.commit()
         assert p.id is not None
-        self.info2phrase[key] = p.id
+        info2phrase[key] = p.id
         return p
 
-    def fetch_versioned_string(self, value: str) -> VersionedString:
+    def fetch_versioned_string(value: str) -> VersionedString:
         """
         Get a versioned string.
         """
-        from recval.model import Phrase, VersionedString
         from recval.database.special_users import importer
         res = VersionedString.query.filter_by(value=value).all()
         assert len(res) in (0, 1)
@@ -85,19 +83,22 @@ class FlaskSQLAlchemyRecordingImporter:
         v = VersionedString.new(value=value, author=importer)
         return v
 
-    def __call__(self, info: RecordingInfo, rec_id: str, recording_path: Path) -> None:
+    def import_recording(info: RecordingInfo, rec_id: str, recording_path: Path) -> None:
         from recval.model import Recording, RecordingSession
         session = RecordingSession.query.filter_by(id=info.session).\
             one_or_none() or RecordingSession.from_session_id(info.session)
-        phrase = self.make_phrase(info)
+        phrase = make_phrase(info)
         # TODO: GENERATE SESSION ID!
+        # TODO: I forgot what a "session" ID is! Like a recording session ID? Why!?
         recording = Recording.new(fingerprint=rec_id,
                                   phrase=phrase,
                                   input_file=recording_path,
                                   session=session,
                                   speaker=info.speaker)
-        self.db.session.add(recording)
-        self.db.session.commit()
+        db.session.add(recording)
+        db.session.commit()
+
+    return import_recording
 
 
 def _initialize(
