9cc6799c517dcf195b25d78d0bbbac2345603f6f
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-17 15:49:54 -0600

    Factor out the model-coupled recording importing stuff.

--- recval/init_db.py
+++ recval/init_db.py
@@ -4,7 +4,13 @@
 """
 Temporary place for database creation glue code.
 """
+import logging
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+from recval.extract_phrases import RecordingExtractor, RecordingInfo
+from recval.transcode_recording import transcode_to_aac
+from recval.recording_session import parse_metadata
 
 
 def initialize(directory: Path) -> None:
@@ -14,6 +20,79 @@ def initialize(directory: Path) -> None:
             app.config['TRANSCODED_RECORDINGS_PATH'],
             app.config['REPOSITORY_ROOT']
     )
+
+
+class RecordingImporter:
+    """
+    Naming things is hard
+    """
+
+    def __init__(self) -> None:
+        from recval.database import init_db
+        self.info2phrase = {}  # type: ignore
+        self.db = init_db()
+
+    if TYPE_CHECKING:
+        from recval.model import Phrase, VersionedString
+
+    def make_phrase(self, info: RecordingInfo) -> Phrase:
+        """
+        Tries to fetch an existing phrase from the database, or else creates a
+        new one.
+        """
+        from recval.model import Phrase, Word, Sentence
+
+        key = info.type, info.transcription
+
+        if key in self.info2phrase:
+            # Look up the phrase.
+            phrase_id = self.info2phrase[key]
+            return Phrase.query.filter_by(id=phrase_id).one()
+
+        # Otherwise, create a new phrase.
+        transcription = self.fetch_versioned_string(info.transcription)
+        translation = self.fetch_versioned_string(info.translation)
+        if info.type == 'word':
+            p = Word(transcription_meta=transcription,
+                     translation_meta=translation)
+        elif info.type == 'sentence':
+            p = Sentence(transcription_meta=transcription,
+                         translation_meta=translation)
+        else:
+            raise Exception(f"Unexpected phrase type: {info.type!r}")
+        assert p.id is None
+        self.db.session.add(p)
+        self.db.session.commit()
+        assert p.id is not None
+        self.info2phrase[key] = p.id
+        return p
+
+    def fetch_versioned_string(self, value: str) -> VersionedString:
+        """
+        Get a versioned string.
+        """
+        from recval.model import Phrase, VersionedString
+        from recval.database.special_users import importer
+        res = VersionedString.query.filter_by(value=value).all()
+        assert len(res) in (0, 1)
+        if res:
+            return res[0]
+        v = VersionedString.new(value=value, author=importer)
+        return v
+
+    def import_recording(self, info: RecordingInfo, rec_id: str, recording_path: Path) -> None:
+        from recval.model import Recording, RecordingSession
+        session = RecordingSession.query.filter_by(id=info.session).\
+            one_or_none() or RecordingSession.from_session_id(info.session)
+        phrase = self.make_phrase(info)
+        # TODO: GENERATE SESSION ID!
+        recording = Recording.new(fingerprint=rec_id,
+                                  phrase=phrase,
+                                  input_file=recording_path,
+                                  session=session,
+                                  speaker=info.speaker)
+        self.db.session.add(recording)
+        self.db.session.commit()
 
 
 def _initialize(
@@ -27,69 +106,17 @@ def _initialize(
     TODO: drastically fix this
     """
 
-    import logging
-
-    from recval.extract_phrases import RecordingExtractor, RecordingInfo
     from recval.model import (Phrase, Recording, Sentence, Word, VersionedString,
                               RecordingSession)
-    from recval.database import init_db
-    from recval.transcode_recording import transcode_to_aac
-    from recval.recording_session import parse_metadata
 
-    info2phrase = {}  # type: ignore
-
-    dest = Path()
+    dest = Path(transcoded_recordings_path)
     metadata_filename = repository_root / 'etc' / 'metadata.csv'
 
     assert directory.resolve().is_dir()
     assert dest.resolve().is_dir()
     assert metadata_filename.resolve().is_file()
 
-    def make_phrase(info: RecordingInfo) -> Phrase:
-        """
-        Tries to fetch an existing phrase from the database, or else creates a
-        new one.
-        """
-
-        key = info.type, info.transcription
-
-        if key in info2phrase:
-            # Look up the phrase.
-            phrase_id = info2phrase[key]
-            return Phrase.query.filter_by(id=phrase_id).one()
-
-        # Otherwise, create a new phrase.
-        transcription = fetch_versioned_string(info.transcription)
-        translation = fetch_versioned_string(info.translation)
-        if info.type == 'word':
-            p = Word(transcription_meta=transcription,
-                     translation_meta=translation)
-        elif info.type == 'sentence':
-            p = Sentence(transcription_meta=transcription,
-                         translation_meta=translation)
-        else:
-            raise Exception(f"Unexpected phrase type: {info.type!r}")
-        assert p.id is None
-        db.session.add(p)
-        db.session.commit()
-        assert p.id is not None
-        info2phrase[key] = p.id
-        return p
-
-    def fetch_versioned_string(value: str) -> VersionedString:
-        """
-        Get a versioned string.
-        """
-        from recval.database.special_users import importer
-        res = VersionedString.query.filter_by(value=value).all()
-        assert len(res) in (0, 1)
-        if res:
-            return res[0]
-        v = VersionedString.new(value=value, author=importer)
-        return v
-
-    # Create the schema.
-    db = init_db()
+    importer = RecordingImporter()
 
     with open(metadata_filename) as metadata_csv:
         metadata = parse_metadata(metadata_csv)
@@ -112,15 +139,4 @@ def _initialize(
                 year=info.session.year
             ))
             assert recording_path.exists()
-
-        session = RecordingSession.query.filter_by(id=info.session).\
-            one_or_none() or RecordingSession.from_session_id(info.session)
-        phrase = make_phrase(info)
-        # TODO: GENERATE SESSION ID!
-        recording = Recording.new(fingerprint=rec_id,
-                                  phrase=phrase,
-                                  input_file=recording_path,
-                                  session=session,
-                                  speaker=info.speaker)
-        db.session.add(recording)
-        db.session.commit()
+        importer.import_recording(info, rec_id, recording_path)
