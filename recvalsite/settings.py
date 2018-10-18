7771bb6f975f691e50e42efab45d647fbf16b688
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-18 11:38:28 -0600

    Create validation app.

--- recvalsite/settings.py
+++ recvalsite/settings.py
@@ -35,6 +35,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Apps defined in this repository.
+    'validation',
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
