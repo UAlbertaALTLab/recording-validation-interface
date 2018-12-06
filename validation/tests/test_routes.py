fa69dd3edb2d5c43ea2cb48e4ac75722b6dbe714
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-06 11:33:18 -0700

    Create test audio file in temporary directory.

--- validation/tests/test_routes.py
+++ validation/tests/test_routes.py
@@ -16,7 +16,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import random
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import pytest  # type: ignore
 from django.shortcuts import reverse  # type: ignore
@@ -39,24 +42,26 @@ def test_serve_recording(client, exported_recording):
     assert page.status_code == 200
     assert page.get('Content-Type') == 'audio/m4a'
     assert page.content == file_contents
+    assert page.content[4:12] == b'ftypM4A ', "Did not serve an .m4a file."
 
 
 @pytest.fixture
-def exported_recording():
+def exported_recording(settings):
     recording = Recipe(Recording, timestamp=random_timestamp).make()
 
     # Create a REAL audio recording, saved on disk.
-    # XXX: bad, temporary, figure out a better way!
-    from django.conf import settings  # type: ignore
-    audio = AudioSegment.empty()
-    filename = settings.RECVAL_AUDIO_DIR / f'{recording.id}.m4a'
-    audio.export(str(filename))
-    file_contents = filename.read_bytes()
+    with TemporaryDirectory() as temp_dir_name:
+        audio_dir = Path(temp_dir_name)
 
-    yield recording, file_contents
+        # Temporarily override the audio directory name.
+        settings.RECVAL_AUDIO_DIR = audio_dir
+        audio = AudioSegment.empty()
+        filename = audio_dir / f'{recording.id}.m4a'
+        # Create an actual, bona fide M4A file.
+        audio.export(os.fspath(filename), format='ipod')
+        file_contents = filename.read_bytes()
 
-    # Clean up!
-    filename.unlink()
+        yield recording, file_contents
 
 
 def random_timestamp():
