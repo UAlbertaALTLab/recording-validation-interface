7cc175ee7f8f2ff977732f7ff0f6e9f3eca8f143
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-20 15:20:11 -0700

    Convert to_indexable_form tests to pytest.parametrize().

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -41,51 +41,64 @@ def test_idempotence(s):
 
 # ############################# Indexing tests ############################# #
 
-def test_index():
+@pytest.mark.parametrize('expected,original', [
+    # Indexing MUST ignore vowel length
+    # Indexing MUST remove extraneous whitespace
+    # Indexing MUST lowercase all the things
+    # Indexing MUST be ASCII
+    # Indexing MUST treat -iw and -ow as the same.
+    # Indexing MUST treat all forms of vowel elision the same.
+
+    # The following are tests I got from some spell-relax .regex file,
+    # deep within giella/.../crk/...
+
     # i (->) %', # short-i elision
-    assert "tanisi" == to_indexable_form("tan'si")
+    ("tanisi", "tan'si"),
 
     # â (->) {ā}, # a + combining macron U+0304
     # ê (->) {ē}, # e + combining macron U+0304
     # î (->) {ī}, # i + combining macron U+0304
     # ô (->) {ō}, # o + combining macron U+0304
-    assert "aeio" == to_indexable_form("a\u0304e\u0304i\u0304o\u0304")
+    ("aeio", "a\u0304e\u0304i\u0304o\u0304"),
 
     # Â (->) {Ā}, # A + combining macron U+0304
     # Ê (->) {Ē}, # E + combining macron U+0304
     # Î (->) {Ī}, # I + combining macron U+0304
     # Ô (->) {Ō}, # O + combining macron U+0304
-    assert "aeio" == to_indexable_form("A\u0304E\u0304I\u0304O\u0304")
+    ("aeio", "A\u0304E\u0304I\u0304O\u0304"),
 
     # â (->) ā, # a macron
     # ê (->) ē, # e macron
     # î (->) ī, # i macron
     # ô (->) ō, # o macron
-    assert "aeio" == to_indexable_form("\u0101\u0113\u012B\u014d")
+    ("aeio", "\u0101\u0113\u012B\u014d"),
 
     # Â (->) Ā, # A macron
     # Ê (->) Ē, # E macron
     # Î (->) Ī, # I macron
     # Ô (->) Ō, # O macron
-    assert "aeio" == to_indexable_form("\u0100\u0112\u012A\u014C")
+    ("aeio", "\u0100\u0112\u012A\u014C"),
 
     # â (->) {â}, # a + combining circumflex accent U+0302
     # ê (->) {ê}, # e + combining circumflex accent U+0302
     # î (->) {î}, # i + combining circumflex accent U+0302
     # ô (->) {ô}, # o + combining circumflex accent U+0302
-    assert "aeio" == to_indexable_form("a\u0302e\u0302i\u0302o\u0302")
+    ("aeio", "a\u0302e\u0302i\u0302o\u0302"),
 
     # Â (->) {Â}, # A + combining circumflex accent U+0302
     # Ê (->) {Ê}, # E + combining circumflex accent U+0302
     # Î (->) {Î}, # I + combining circumflex accent U+0302
     # Ô (->) {Ô}, # O + combining circumflex accent U+0302
-    assert "aeio" == to_indexable_form("A\u0302E\u0302I\u0302O\u0302")
+    ("aeio", "A\u0302E\u0302I\u0302O\u0302"),
 
-    # NS 152 materials consistantly write some vowels as long where Arok's
-    # write them as short. E.G. NS 152 give 'askîy' and Arok gives 'askiy.'
-    assert "askiy" == to_indexable_form("askîy")
+    # # NS 152 materials consistantly (sic) write some vowels as long where Arok's
+    # # write them as short. E.G. NS 152 give 'askîy' and Arok gives 'askiy.'
+    ("askiy", "askîy"),
 
     # # Explanation:
     # # lexical side (->) input
     # # Or, in other words:
     # # correct (->) in use out there
+])
+def test_index(expected, original):
+    assert to_indexable_form(original) == expected
