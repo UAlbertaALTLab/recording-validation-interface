b4975d988a6b0c84ed088ca7b1efdaddc8bc9fd1
Eddie Antonio Santos <easantos@ualberta.ca>
2020-08-05 09:20:53 -0600

    change: remove RECVAL_AUDIO_DIR

--- recvalsite/settings.py
+++ recvalsite/settings.py
@@ -168,10 +168,6 @@ STATIC_URL = config("SCRIPT_NAME", default="") + "/static/"
 # Remember to run manage.py collectstatic!
 STATIC_ROOT = config("STATIC_ROOT", default="/var/www/recvalsite/static")
 
-# DEPRECATED: use MEDIA_ROOT + RECVAL_AUDIO_PREFIX instead!
-# Where the transcoded audio files should be placed AND served from.
-RECVAL_AUDIO_DIR = config("RECVAL_AUDIO_DIR", BASE_DIR / "data" / "audio", cast=Path)
-
 # This is concatenated with MEDIA_ROOT and MEDIA_URL to store and serve the audio files.
 RECVAL_AUDIO_PREFIX = config("RECVAL_AUDIO_PREFIX", default="audio/")
 
