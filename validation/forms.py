066c9f93abc8d5475809a0f5818b54ffc01865ec
nienna73 <jcpoulin@ualberta.ca>
2021-01-22 09:18:30 -0700

    wip: added some styles, more to come

--- validation/forms.py
+++ validation/forms.py
@@ -2,8 +2,18 @@ from django import forms
 
 
 class Login(forms.Form):
-    username = forms.CharField(required=False, widget=forms.TextInput)
-    password = forms.CharField(required=False, widget=forms.PasswordInput)
+    username = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "style": "max-width: 15rem;"}
+        ),
+    )
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "style": "max-width: 15rem;"}
+        ),
+    )
 
 
 class Register(forms.Form):
