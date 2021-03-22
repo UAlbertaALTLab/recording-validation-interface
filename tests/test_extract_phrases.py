484ad928650069d4b033c43adefbc6be1ceab705
Eddie Antonio Santos <eddieantonio@hey.com>
2021-03-22 09:18:18 -0600

    docs: remove shebang + document

--- tests/test_extract_phrases.py
+++ tests/test_extract_phrases.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-
 import pytest
 
 from librecval.extract_phrases import InvalidFileName, get_mic_id
 
 
 def test_invalid_mic_id():
+    """
+    Should raise InvalidFileName when an Track ID cannot be parsed.
+    """
     with pytest.raises(InvalidFileName):
         get_mic_id("💩.eaf")
