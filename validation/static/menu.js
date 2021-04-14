66b8ecbf8a6e13a93bf9138ba2f6843e7967aa3c
Jolene Poulin <jcpoulin@ualberta.ca>
2021-04-14 10:47:34 -0600

    wip: changed burger to caret

--- validation/static/menu.js
+++ validation/static/menu.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function showMenu() {
-    let menu = document.getElementById("burger-menu");
+    let menu = document.getElementById("options-menu");
     if (menu.classList.contains("menu__none")) {
         menu.classList.remove("menu__none");
         menu.classList.add("menu__block");
@@ -13,7 +13,7 @@ function showMenu() {
 
 function showMacron(option) {
     document.cookie = "macron=" + option + "; SameSite=Lax;"
-    let menu = document.getElementById("burger-menu");
+    let menu = document.getElementById("options-menu");
     menu.classList.add("menu__none");
     menu.classList.remove("menu__block");
     location.reload();
