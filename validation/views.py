da93ac1da136a96bccb0a62e85012695aae3c256
Jolene Poulin <jcpoulin@ualberta.ca>
2021-04-23 16:03:44 -0600

    feat: only validate spelling on new words

--- validation/views.py
+++ validation/views.py
@@ -66,11 +66,7 @@ def index(request):
         if is_linguist:
             all_phrases = Phrase.objects.all()
         else:
-            # Doing an "exclude" here instead of a "filter" ensures
-            # that at least a few results are displayed
-            all_phrases = Phrase.objects.exclude(origin="MD").exclude(
-                status="auto-validated"
-            )
+            all_phrases = Phrase.objects.exclude(status="auto-validated")
     else:
         all_phrases = Phrase.objects.filter(status=mode)
 
