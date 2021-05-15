fa048d1a632f761130620a9be13788f17dc2bb54
Jolene Poulin <jcpoulin@ualberta.ca>
2021-05-14 19:45:49 -0600

    wip: moved edit segment js to its own file

---
+++ validation/static/editSegment.js
@@ -0,0 +1,44 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let button of document.querySelectorAll('[data-segment-name]')) {
+        button.addEventListener("click", async (e) => {
+            const name = e.target.dataset.segmentName
+
+            document.getElementById('edit').style.display = "block";
+            document.getElementById('id_cree').focus()
+
+            const translation = document.getElementById(name + '-translation').innerHTML;
+
+            document.getElementById('id_cree').value = name;
+            document.getElementById('id_translation').value = translation;
+        })
+    }
+
+    for (let button of document.querySelectorAll('[data-cy="accept-button"], [data-cy="revert-button"]')) {
+        button.addEventListener("click", async(e) => {
+            document.getElementById('edit').style.display = "block";
+            document.getElementById('id_cree').focus()
+
+            const name = e.target.dataset.transcription
+            const translation = e.target.dataset.translation
+            const analysis = e.target.dataset.analysis
+
+            document.getElementById('id_cree').value = name;
+            document.getElementById('id_translation').value = translation;
+            document.getElementById('id_analysis').value = analysis;
+        })
+    }
+
+    for (let button of document.querySelectorAll('[data-cy="cancel-button"]')) {
+        button.addEventListener("click", async(e) => {
+            document.getElementById('edit').style.display = "none";
+        })
+    }
+
+    for (let button of document.querySelectorAll('[data-cy="back-button"]')) {
+        button.addEventListener("click", async(e) => {
+            window.history.back()
+        })
+    }
+})
