5a02438dcb7b9e1c82d93c5a9ca7866068b7162b
Jolene Poulin <jcpoulin@ualberta.ca>
2021-05-17 14:43:26 -0600

    remove: unused function

--- validation/static/menu.js
+++ validation/static/menu.js
@@ -14,16 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-function showMenu() {
-    let menu = document.getElementById("options-menu");
-    if (menu.classList.contains("menu__none")) {
-        menu.classList.remove("menu__none");
-        menu.classList.add("menu__block");
-    } else {
-        menu.classList.add("menu__none");
-        menu.classList.remove("menu__block");
-    }
-}
 
 function showCircumflex(option) {
     document.cookie = "macron=" + option + "; SameSite=Lax;"
