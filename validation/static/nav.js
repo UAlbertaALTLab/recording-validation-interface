3ee4369ffe8ac7d5d8dc7cce35a05dce08b0d897
Jolene Poulin <jcpoulin@ualberta.ca>
2021-06-04 11:34:21 -0600

    fix: use URLSearchParams

--- validation/static/nav.js
+++ validation/static/nav.js
@@ -4,26 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let b of document.querySelectorAll('[data-cy="go-button-nav"]')) {
         b.addEventListener("click", (e) => {
             let pageNo = document.querySelector('[data-cy="page-num-input"]').value;
-            console.log(pageNo);
-            let prevPageNo = getUriComponent("page");
-            let loc = window.location.search;
-            if (loc && prevPageNo) {
-                let prevPage = "page=" + prevPageNo;
-                let newPage = "page=" + pageNo;
-                loc = loc.replace(prevPage, newPage);
-            } else if (loc) {
-                loc += "&page=" + pageNo;
-            } else {
-                loc += "?page=" + pageNo;
+            if (!pageNo) {
+                return;
             }
 
-            window.location = loc;
+            let searchParams = new URLSearchParams(window.location.search);
+            searchParams.set("page", pageNo)
+            window.location.search = searchParams.toString();
         })
     }
 })
-
-// https://stackoverflow.com/questions/831030/how-to-get-get-request-parameters-in-javascript
-function getUriComponent(name){
-   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-      return decodeURIComponent(name[1]);
-}
