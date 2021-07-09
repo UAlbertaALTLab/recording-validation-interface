6ba12321dad51db8d33b741d4f79dcc78684a2f4
Jolene Poulin <jcpoulin@ualberta.ca>
2021-07-09 11:50:38 -0600

    wip: extract speaker bios

---
+++ librecval/extract_speaker_bios.py
@@ -0,0 +1,24 @@
+from pympi.Elan import Eaf
+from pathlib import Path
+
+
+def extra_speaker_bios():
+    path_to_eaf_files = Path()
+    for file in path_to_eaf_files:
+        # extract recordings
+        # extract annotations
+        get_recording_cree(file)
+        get_recording_english(file)
+        get_annotation_english(file)
+
+
+def get_recording_cree(path):
+    pass
+
+
+def get_recording_english(path):
+    pass
+
+
+def get_annotation_english(path):
+    pass
