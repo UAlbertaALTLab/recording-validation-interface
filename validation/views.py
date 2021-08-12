3bf2c29e3fbb01f976e238aa1f3077ad45f20163
nienna73 <jcpoulin@ualberta.ca>
2021-08-12 16:45:58 -0600

    fix: add timezone
    Co-authored-by: andrewdotn <andrew@neitsch.ca>

--- validation/views.py
+++ validation/views.py
@@ -958,7 +958,7 @@ def save_metadata_to_file(rec_id, user, transcription, translation):
         "user_id": user.id,
         "username": user.username,
         "full_name": user.first_name + " " + user.last_name,
-        "recorded_on": str(datetime.datetime.now()),
+        "recorded_on": str(datetime.datetime.now().astimezone()),
         "transcription": transcription,
         "translation": translation,
         "dialect": "Plains Cree",
