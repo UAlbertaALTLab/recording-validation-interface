dd288494e3f4d11e76b6576c1260fef416d04f53
Jolene Poulin <jcpoulin@ualberta.ca>
2022-03-09 11:33:22 -0700

    add: process to import Jean's bio

---
+++ validation/management/commands/importjeanbio.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Copyright 2018 Eddie Antonio Santos <easantos@ualberta.ca>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+Django management command to import recordings. This assumes the database has
+been created.
+Its defaults are configured using the following settings:
+    MEDIA_ROOT
+    RECVAL_AUDIO_PREFIX
+    RECVAL_METADATA_PATH
+    RECVAL_SESSIONS_DIR
+See recvalsite/settings.py for more information.
+"""
+
+from django.core.management.base import BaseCommand  # type: ignore
+
+from validation.models import Speaker
+
+
+class Command(BaseCommand):
+    help = "imports Jean's bios into the database"
+
+    def handle(self, *args, **kwargs) -> None:
+        speaker = Speaker.objects.filter(code="OKI")
+        with open("/app/data/okimasis-bios/jean_bio_cree.txt") as f:
+            content = f.read()
+            speaker.source_bio_text = content
+
+        with open("/app/data/okimasis-bios/jean_bio_cree.txt") as f:
+            content = f.read()
+            speaker.target_bio_text = content
+
+        speaker.save()
