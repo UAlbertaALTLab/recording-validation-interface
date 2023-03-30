856a7c07307d8e2ca493d70a92867d0d5368b780
Jolene Poulin <jcpoulin@ualberta.ca>
2023-03-30 17:26:13 -0600

    attempt to replace combined characters with single characters

--- validation/management/commands/lookforcombinedcharacters.py
+++ validation/management/commands/lookforcombinedcharacters.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand  # type: ignore
 
-from validation.models import Phrase
+from validation.models import Phrase, LanguageVariant
 
 
 class Command(BaseCommand):
@@ -8,5 +8,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options) -> None:
         phrases = Phrase.objects.all()
+        language = LanguageVariant.objects.get(code="moswacihk")
         for phrase in phrases:
-            print(phrase.transcription.find(u"ˆ"))
+            if phrase.language == language:
+                if phrase.transcription.find("ê"):
+                    phrase.transcription = phrase.transcription.replace("ê", "ê")
+                if phrase.transcription.find("â"):
+                    phrase.transcription = phrase.transcription.replace("â", "â")
+                if phrase.transcription.find("î"):
+                    phrase.transcription = phrase.transcription.replace("î", "î")
+                if phrase.transcription.find("ô"):
+                    phrase.transcription = phrase.transcription.replace("ô", "ô")
+            phrase.save()
