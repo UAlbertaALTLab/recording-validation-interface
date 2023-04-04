d172ce46f1677eeed44f0817c06513f1db116432
Jolene Poulin <jcpoulin@ualberta.ca>
2023-04-04 17:12:47 -0600

    change: use unicode, not ascii

--- validation/management/commands/lookforcombinedcharacters.py
+++ validation/management/commands/lookforcombinedcharacters.py
@@ -9,38 +9,49 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options) -> None:
         """
-        For some reason, the system recognizes the dual character î as the same thing as the
-        single character î, even when they're different. It's infuriating and I've spent a long time
-        trying to fix this error.
-        So, we can compare them as the same, but we need to turn the string into an ascii object first
-        We know we have a special character if it gets ascii encoded as '?', or ascii value 63
-        We store the indices of all the 63's, then construct a new string where we replace
-        these dual character special characters with the single character equivalent. Actually, we just
-        replace all the special characters with their equivalent.
-        It's convoluted, but it should work.
+        Many of the characters are combined characters, ˆ+e instead of ê. We need them to be one character
+        instead of 2. So, we convert the string to unicode, which represents the joining ˆ as \xcc\x82.
+        We replace those characters with the correct single character, which appears as the character before
+        the \xcc\x82. That is, the combined "ahâm" is represented as "'aha\\xcc\\x82m'", so we replace the
+        "a\\xcc\\x82" with "â"
         """
         language = LanguageVariant.objects.get(code="moswacihk")
         phrases = Phrase.objects.filter(language=language)
         for phrase in tqdm(phrases):
-            ascii_phrase = phrase.transcription.encode("ascii", "replace")
-            i = 0  # index in string
+            unicode_phrase = repr(phrase.transcription)
+            i = 0  # index in unicode_phrase
+            j = 0  # index in phrase.transcription
             new_phrase = ""
-            for c in ascii_phrase:
-                char = phrase.transcription[i]
-                if c == "63":
-                    if char == "ê":
-                        new_phrase += "ê"
-                    elif char == "â":
-                        new_phrase += "â"
-                    elif char == "î":
-                        new_phrase += "î"
-                    elif char == "ô":
-                        new_phrase += "ô"
-                    else:
-                        # maybe we actually have a '?'
-                        new_phrase += char
+            while i < len(unicode_phrase):
+                c = unicode_phrase[i]
+                if c == "'":
+                    i += 1
                 else:
-                    new_phrase += char
-                i += 1
+                    char = phrase.transcription[j]
+                    j += 1
+                    if (i + 1) < len(unicode_phrase):
+                        next_char = unicode_phrase[i + 1]
+                        if next_char == "\\":
+                            if c == "e":
+                                new_phrase += "ê"
+                            elif c == "a":
+                                new_phrase += "â"
+                            elif c == "i":
+                                new_phrase += "î"
+                            elif c == "o":
+                                new_phrase += "ô"
+                            # the next char is \
+                            # the sequence we're looking at it \xcc\x82
+                            # we want to jump the rest of this sequence
+                            # so we increase by 9 to get to the next character
+                            i += 9
+                        else:
+                            # the next character isn't the special character
+                            new_phrase += char
+                            i += 1
+                    else:
+                        # don't forget the last character!
+                        new_phrase += char
+                        i += 1
             phrase.transcription = new_phrase
             phrase.save()
